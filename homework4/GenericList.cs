43e8f06cea342e008b083acfff5cdd54d7e6a59c
KimtaetaeV <79618994+KimtaetaeV@users.noreply.github.com>
2021-03-21 10:54:04 +0800

    Add files via upload

---
+++ homework4/GenericList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4
+{
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            head = null;
+            tail = null;
+
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public Node<T> Tail
+        {
+            get => tail;
+        }
+
+
+        public void addNode(T data) {
+            Node<T> node = new Node<T>(data);
+            if (head == null) { head = tail = node; }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+        }
+
+
+        public void Foreach(Action<T>action){
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                action(temp.Data);
+                temp = temp.Next;
+            }
+        }
+        
+    }
+}
---
+++ homework4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4
+{
+   
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            GenericList<int> intList = new GenericList<int>();
+            Random Ram = new Random();
+            for(int i = 0; i < 6; i++)
+            {
+                int ran = Ram.Next(2, 10);
+                intList.addNode(ran);
+            }
+
+            int minTemp = 10;
+            int maxTemp = 0;
+            Action<int> action1 = (int s) => { Console.Write(s); };
+            Action<int> action2 = (int s) => { maxTemp = s > maxTemp ? s:maxTemp; };
+            Action<int> action3 = s => { minTemp = s < minTemp ? s : minTemp; };
+            Console.WriteLine("遍历泛型链表：");
+            intList.Foreach(action1);
+            intList.Foreach(action2);
+            intList.Foreach(action3);
+            Console.WriteLine($"\n最大值为{maxTemp}");
+            Console.WriteLine($"最小值为{minTemp}");
+            Console.ReadLine();
+
+        }
+    }
+}
